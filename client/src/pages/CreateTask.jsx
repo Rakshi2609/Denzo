d5523bae947829d3c258fd5cba1a4ea5f9053ec1
Rakshith Ganjimut <rakshithganjimut@gmail.com>
2026-01-02 16:19:49 +0530

    Change button label from 'Create Task' to 'Create'

--- client/src/pages/CreateTask.jsx
+++ client/src/pages/CreateTask.jsx
@@ -240,8 +240,8 @@ export default function CreateTask() {
                   </>
                 ) : (
                   <>
-                    <FaSave />
-                    Create Task
+                    {/* <FaSave /> */}
+                    Create
                   </>
                 )}
               </motion.button>
